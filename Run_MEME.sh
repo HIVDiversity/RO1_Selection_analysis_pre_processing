58b8d89882589f1b5d959168165fae5f537f0a7c
ayssel-uct <75947720+ayssel-uct@users.noreply.github.com>
2022-11-17 17:22:29 +0200

    Add files via upload

---
+++ Run_MEME.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+declare -a array=( $(ls 04_Lineage_partitioned_NT_alignments/V703*/V703*/*na*_filtered.fasta) )
+
+arraylength=${#array[@]}
+
+for ((i=1; i<${arraylength}; i++));
+do
+
+x=${array[$i]} #full path to a single input fasta file used in step 1.
+treefile=${x%%.fasta}.nwk
+
+hyphy meme ${x} ${treefile} #runs hyphy and if all goes well meme saves the outut to the same directory taht conatin sthe fasta file/ treefile
+
+done
