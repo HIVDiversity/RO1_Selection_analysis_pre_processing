28c4c822648064063c00ddb4841e2ce274833a7f
ayssel-uct <75947720+ayssel-uct@users.noreply.github.com>
2023-12-04 19:54:35 +0200

    Update HyPhy_remove_stops_duplicates.sh

--- HyPhy_remove_stops_duplicates.sh
+++ HyPhy_remove_stops_duplicates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a array=( $(ls 04_Lineage_partitioned_NT_alignments/V703*/V703*/*na*.fasta) )
+declare -a array=( $(ls Morgane_codon_aligned_lineages/70*_Codon_aligned_lineages/V70*.na/V70*/V70*_*na*.fasta) )
 
 arraylength=${#array[@]}
 
@@ -14,5 +14,6 @@ x=${array[$i]}
 #If I wanted to keep only unique sequences and discard duplicates i need to change it to (3):[Yes/No] Filter duplicate sequences but keep all sites
 
 hyphy rmv "Universal" ${x} "Disallow stops" ${x%%.fasta}_filtered.fasta #Removes stop codons and duplicates and writes to a new file with the extension "filtered.fasta"
+echo ${i}
 done
 
